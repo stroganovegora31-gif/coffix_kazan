0c34aa64c2726f4099d6ba76737cb448e8a2cb3d
stroganovegora31-gif <stroganovegora31@gmail.com>
2026-02-17 01:00:52 +0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3104506957"
+    serviceWorkerVersion: "4114072131"
   }
 });
